6006cfe3391860e4fe039debd8a65b4e3d294f0a
tmsoft12 <tmsoft12@gmail.com>
2024-10-23 12:43:50 +0500

    full

--- play.py
+++ play.py
@@ -9,16 +9,15 @@ class MusicPlayer:
     def play(self):
         pygame.mixer.music.load(self.music_file)
         pygame.mixer.music.play()
-        print("Müzik çalıyor...")
+        print("Baslydy...")
 
     def stop(self):
         pygame.mixer.music.stop()
-        print("Müzik durduruldu.")
+        print("Durdy.")
 
 if __name__ == "__main__":
-    # Müzik dosyasının yolunu buraya ekleyin
     player = MusicPlayer('ses.mp3')
 
     player.play()
-    time.sleep(10)  # 10 saniye müzik çalsın
+    time.sleep(10) 
     player.stop()
